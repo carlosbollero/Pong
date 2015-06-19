b0f184f190336593bed4bc10b81ae826c6ae878c
Carlos <carlosmbollero@gmail.com>
2015-06-19 17:10:26 -0300

    Added file, ball moving, paddle working

---
+++ animation.js
@@ -0,0 +1,111 @@
+
+var canvas;
+var canvasContext;
+
+const FRAMESPERSECOND = 50;
+
+const PADDLE_HEIGHT = 100;
+const PADDLE_WIDHT = 10;
+const PADDLEPOSX = 7;
+var paddlePosY = 250;
+
+const BALLRADIUS = 5;
+var ballWidth = 10;
+var ballHeight = 10;
+var ballRadius = 5;
+var ballPosX;
+var ballPosY;
+var ballSpeedX = 5;
+var ballSpeedY = 5;
+
+function drawBall() {
+    
+    console.log("pos (" + ballPosX + "," + ballPosY + ")");
+    console.log("size : " + ballWidth + "x" + ballHeight);
+    drawCircle(ballPosX, ballPosY, BALLRADIUS, 'green');
+}
+
+function drawCircle(centerX, centerY, radius, color){
+    canvasContext.fillStyle = color;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+    canvasContext.fill();
+}
+
+function drawScenario() {
+    drawRect(0, 0, canvas.width, canvas.height, 'black');
+}
+
+function drawPaddles(){
+    drawRect(PADDLEPOSX, paddlePosY, PADDLE_WIDHT, PADDLE_HEIGHT, 'white');   
+}
+
+function drawRect(x, y, width, height, color) {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, width, height);
+}
+
+function draw() {
+    drawScenario();
+    drawPaddles();
+    drawBall();
+}
+
+function moveBall() {
+    
+    ballPosX = ballPosX + ballSpeedX;
+    ballPosY = ballPosY + ballSpeedY;
+    
+    if (ballPosX > canvas.width - ballWidth){
+        ballSpeedX = -ballSpeedX;
+    }
+    if ( ballPosX < 0 + ballWidth) {
+        ballSpeedX = -ballSpeedX;   
+    }
+    
+    if (ballPosY > canvas.height - ballHeight){
+        ballSpeedY = -ballSpeedY;
+    }
+    if (ballPosY < 0 + ballHeight){
+        ballSpeedY = -ballSpeedY;   
+    }
+}
+
+function getMousePos(event){
+    var root = document.documentElement;
+    var rect = canvas.getBoundingClientRect();
+    var mousePosX = event.clientX - rect.left - root.scrollLeft;
+    var mousePosY = event.clientY - rect.top - root.scrollTop;
+    
+    return {
+        x: mousePosX,
+        y: mousePosY
+    };
+    
+}
+
+function update() {
+    moveBall();
+    draw();
+}
+
+window.onload = function () {
+    
+    console.log('init');
+    canvas = document.getElementById('gameCanvas');
+    canvasContext =  canvas.getContext('2d');
+    
+    ballPosX = canvas.width / 2 - ballWidth / 2;
+    ballPosY = canvas.height / 2 - ballHeight / 2;
+    
+    /*update();*/
+    setInterval(update, 1000 / FRAMESPERSECOND);
+    
+    canvas.addEventListener('mousemove', 
+                function (event){
+                    var mousePos = getMousePos(event);
+                    paddlePosY = mousePos.y - PADDLE_HEIGHT/2;
+    });
+    
+    console.log('stop');
+};
