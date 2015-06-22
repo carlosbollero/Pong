110c4b36aaeddadc752398b1518c2983e7512e93
Carlos <carlosmbollero@gmail.com>
2015-06-22 17:52:20 -0300

    cleanning the code

--- animation.js
+++ animation.js
@@ -7,16 +7,16 @@ var canvasHeight = window.innerHeight;*/
 const FRAMESPERSECOND = 50;
 
 const PADDLE_HEIGHT = 100;
-const PADDLE_WIDHT = 10;
+const PADDLE_WIDTH = 10;
 const PADDLE_PADDING = 20;
 const PADDLE_POS_X = 0 + PADDLE_PADDING;
-const PADDLE_2_POS_X = 800 - (PADDLE_PADDING + PADDLE_WIDHT);
+const PADDLE_2_POS_X = 800 - (PADDLE_PADDING + PADDLE_WIDTH);
 var paddlePosY = 250;
 var paddle2PosY = 250;
 
 const BALL_RADIUS = 7;
-const BALL_INITAL_X_SPEED = 8;
-const BALL_INITAL_Y_SPEED = 5;
+const BALL_INITAL_X_SPEED = 3;
+const BALL_INITAL_Y_SPEED = 3;
 var ballPosX;
 var ballPosY;
 var ballSpeedX;
@@ -28,7 +28,7 @@ var points = 0;
 
 function drawBall() {
     
-    console.log("pos (" + ballPosX + "," + ballPosY + ")");
+    // console.log("pos (" + ballPosX + "," + ballPosY + ")");
     drawCircle(ballPosX, ballPosY, BALL_RADIUS, 'green');
     
 }
@@ -38,31 +38,29 @@ function drawCircle(centerX, centerY, radius, color){
     canvasContext.beginPath();
     canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
     canvasContext.fill();
+
 }
 
 function drawField() {
     drawRect(0, 0, canvas.width, canvas.height, 'black');
     for (var i = 0; i < canvas.height / 20; i++){
         var height = canvas.height /20 - 15;
-        var lastPos = i * canvas.height / 20;
-        
-        drawRect(canvas.width/2,lastPos,1,height,'white');
-        
+        var lastPos = i * canvas.height / 20;        
+        drawRect(canvas.width/2,lastPos,1,height,'white');        
     }
 }
 
 function drawScorer() {
-    canvasContext.fillStlye = "white";
+    canvasContext.fillStyle = "white";
 	canvasContext.font = "16px Arial, sans-serif";
 	canvasContext.textAlign = "left";
 	canvasContext.textBaseline = "top";
 	canvasContext.fillText("Score: " + points, 60, 30 );
-    console.log("hey");
 }
 
 function drawPaddles(){
-    drawRect(PADDLE_POS_X, paddlePosY, PADDLE_WIDHT, PADDLE_HEIGHT, 'white');
-    drawRect(PADDLE_2_POS_X, paddle2PosY, PADDLE_WIDHT, PADDLE_HEIGHT, 'white');
+    drawRect(PADDLE_POS_X, paddlePosY, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
+    drawRect(PADDLE_2_POS_X, paddle2PosY, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
 }
 
 function drawRect(x, y, width, height, color) {
@@ -73,42 +71,47 @@ function drawRect(x, y, width, height, color) {
 function draw() {
     
     drawField();
-    drawScorer();
     drawPaddles();
     drawBall();
+    drawScorer();
 }
 
 function moveBall() {
     
-    ballPosX += ballSpeedX;
-    ballPosY += ballSpeedY;
     
-    if (ballPosX > canvas.width - BALL_RADIUS){
-        ballSpeedX = -ballSpeedX;
-        gameReset();
-    }
-    
-    if ( ballPosX < 0 + BALL_RADIUS) {
-        ballSpeedX = -ballSpeedX;
-        gameReset();
-    }
-    
-    if ((ballPosX + BALL_RADIUS >= PADDLE_2_POS_X) && ((ballPosY > paddle2PosY) && (ballPosY < paddle2PosY + PADDLE_HEIGHT))){
-        ballSpeedX = -ballSpeedX;   
-    }
-    
-    if ((ballPosX - BALL_RADIUS <= PADDLE_POS_X + PADDLE_WIDHT) && ((ballPosY > paddlePosY) && (ballPosY < paddlePosY + PADDLE_HEIGHT))){
+    if (((ballPosX - BALL_RADIUS) <= (PADDLE_POS_X + PADDLE_WIDTH)) && ((ballPosY > paddlePosY) && (ballPosY < paddlePosY + PADDLE_HEIGHT))){
+        console.log('paddle : (' + (PADDLE_POS_X + PADDLE_WIDTH) + ')');
+        console.log('ball: (' + (ballPosX - BALL_RADIUS) + ')' );
         ballSpeedX = -ballSpeedX;
         ballSpeedX++;
         points++;
     }
+
+    if ((ballPosX + BALL_RADIUS >= PADDLE_2_POS_X) && ((ballPosY > paddle2PosY) && (ballPosY < paddle2PosY + PADDLE_HEIGHT))){
+        console.log('paddle : (' + PADDLE_2_POS_X + ')');
+        console.log('ball: (' + (ballPosX + BALL_RADIUS) + ')' );
+        ballSpeedX = -ballSpeedX;   
+    }
+
+    if (ballPosX + BALL_RADIUS > canvas.width ){
+        ballSpeedX = -ballSpeedX;
+        gameReset();
+    }
     
-    if (ballPosY > canvas.height - BALL_RADIUS){
+    if ( ballPosX - BALL_RADIUS < 0) {
+        ballSpeedX = -ballSpeedX;
+        gameReset();
+    }
+    
+    if (ballPosY + BALL_RADIUS >= canvas.height){
         ballSpeedY = -ballSpeedY;
     }
-    if (ballPosY < 0 + BALL_RADIUS){
+    if (ballPosY - BALL_RADIUS <= 0){
         ballSpeedY = -ballSpeedY;   
     }
+
+    ballPosX += ballSpeedX;
+    ballPosY += ballSpeedY;
 }
 
 function AIPaddleMovement(){
@@ -122,9 +125,14 @@ function AIPaddleMovement(){
 
 function ballReset() {
     ballPosX = canvas.width / 2;
+    console.log(ballPosX);
     ballPosY = canvas.height / 2;
     ballSpeedX = BALL_INITAL_X_SPEED;
     ballSpeedY = BALL_INITAL_Y_SPEED;
+    if (Math.random() > 0.5) {
+        ballSpeedY = -ballSpeedY;
+    }
+
 }
 
 function scorerReset(){
